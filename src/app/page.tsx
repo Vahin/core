b6b8a1f5c0e5510fe91b6df7ace2dfe6fcb0c9b2
Vahin <danil.e.vakhrushev@gmail.com>
2024-10-12 21:25:20 +0500

    test ci/cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1>New Course</h1>
+      <h1>Courses</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
       />
-      <h2>Courses List</h2>
       <CoursesList revalidatePagePath="/" />
     </main>
   );
